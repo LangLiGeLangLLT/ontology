c85e86df3e9a6f76731f73c608572e103313b1a1
llt <418797644@qq.com>
2026-09-20 18:49:33 +0800

    update

--- ecom/main.py
+++ ecom/main.py
@@ -1,1 +1,117 @@
-print("hello world")
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, END, START, MessagesState
+from langgraph.graph.message import add_messages
+from langchain.messages import SystemMessage, ToolMessage, HumanMessage
+from langchain.tools import tool
+from typing import Literal
+
+load_dotenv()
+
+model = ChatOpenAI(
+    model=os.getenv("LLM_MODEL_ID"),
+    api_key=os.getenv("LLM_API_KEY"),
+    base_url=os.getenv("LLM_BASE_URL"),
+    temperature=0,
+)
+
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a * b
+
+
+@tool
+def add(a: int, b: int) -> int:
+    """Adds `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a + b
+
+
+@tool
+def divide(a: int, b: int) -> float:
+    """Divide `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a / b
+
+
+tools = [add, multiply, divide]
+tools_by_name = {tool.name: tool for tool in tools}
+model_with_tools = model.bind_tools(tools)
+
+
+def llm_call(state: MessagesState):
+    """LLM decides whether to call a tool or not"""
+
+    return {
+        "messages": [
+            model_with_tools.invoke(
+                [
+                    SystemMessage(
+                        content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+                    )
+                ]
+                + state["messages"]
+            )
+        ],
+    }
+
+
+def tool_node(state: dict):
+    """Performs the tool call"""
+
+    result = []
+    for tool_call in state["messages"][-1].tool_calls:
+        tool = tools_by_name[tool_call["name"]]
+        observation = tool.invoke(tool_call["args"])
+        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+    return {"messages": result}
+
+
+def should_continue(state: MessagesState) -> Literal["tool_node", END]:
+    """Decide if we should continue the loop or stop based upon whether the LLM made a tool call"""
+
+    messages = state["messages"]
+    last_message = messages[-1]
+
+    if last_message.tool_calls:
+        return "tool_node"
+
+    return END
+
+
+def create_ecom_agent():
+    agent_builder = StateGraph(MessagesState)
+
+    agent_builder.add_node("llm_call", llm_call)
+    agent_builder.add_node("tool_node", tool_node)
+
+    agent_builder.add_edge(START, "llm_call")
+    agent_builder.add_conditional_edges("llm_call", should_continue, ["tool_node", END])
+    agent_builder.add_edge("tool_node", "llm_call")
+
+    agent = agent_builder.compile()
+    return agent
+
+
+if __name__ == "__main__":
+    agent = create_ecom_agent()
+    messages = [HumanMessage(content="Add 3 and 4.")]
+    response = agent.invoke({"messages": messages})
+    for m in response["messages"]:
+        m.pretty_print()
